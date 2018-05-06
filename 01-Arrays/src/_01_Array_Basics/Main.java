57e0d94e2e96fe269df37296fdcff96d09139603
xiaolinzi <daoshangxl@163.com>
2018-05-06 16:14:03 +0800

    使用java本身提供的数组

---
+++ 01-Arrays/src/_01_Array_Basics/Main.java
@@ -0,0 +1,25 @@
+package _01_Array_Basics;
+
+/**
+ * 使用 java 本身提供的数组
+ * <p>
+ * Created by xiaolinzi on 2018/5/6.
+ */
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = i;
+
+        int[] scores = new int[]{100, 99, 66};
+        for (int i = 0; i < scores.length; i++)
+            System.out.println(scores[i]);
+
+        for (int score : scores)
+            System.out.println(score);
+
+        scores[0] = 90;
+        for (int score : scores)
+            System.out.println(score);
+    }
+}
