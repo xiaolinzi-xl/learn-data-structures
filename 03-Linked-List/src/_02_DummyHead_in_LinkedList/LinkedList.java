2a9e727a36e130e126996e7006a3d9ca007c957a
xiaolinzi <daoshangxl@163.com>
2018-07-13 08:33:21 +0800

    增加虚拟节点

---
+++ 03-Linked-List/src/_02_DummyHead_in_LinkedList/LinkedList.java
@@ -0,0 +1,57 @@
+package _02_DummyHead_in_LinkedList;
+
+/**
+ * Created by xiaolinzi on 2018/7/13.
+ */
+public class LinkedList<E> {
+
+    private class Node {
+        private E e;
+        private Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+    }
+
+    private Node dummyHead;
+    private int size;
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void add(int index, E e) {
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("Add failed.Index illegal.");
+
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++)
+            prev = prev.next;
+
+        prev.next = new Node(e, prev.next);
+        size++;
+    }
+
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+}
