ad346e4d7a0eaf5ae9d7cba74e3b96dfc3edf365
xiaolinzi <daoshangxl@163.com>
2018-08-05 19:16:09 +0800

    定义map接口

---
+++ 06-Set-and-Map/src/_04_map_basics/Map.java
@@ -0,0 +1,20 @@
+package _04_map_basics;
+
+/**
+ * Created by xiaolinzi on 2018/8/5.
+ */
+public interface Map<K, V> {
+    void add(K key, V value);
+
+    V remove(K key);
+
+    boolean contains(K key);
+
+    V get(K key);
+
+    void set(K key, V newValue);
+
+    int getSize();
+
+    boolean isEmpty();
+}
