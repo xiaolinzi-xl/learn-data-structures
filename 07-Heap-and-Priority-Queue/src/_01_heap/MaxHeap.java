1f3bda59368f771ab3e81f08a514ffff3ed753ad
xiaolinzi <daoshangxl@163.com>
2018-08-11 16:02:07 +0800

    最大堆：replace 和 heapify

--- 07-Heap-and-Priority-Queue/src/_01_heap/MaxHeap.java
+++ 07-Heap-and-Priority-Queue/src/_01_heap/MaxHeap.java
@@ -15,6 +15,12 @@ public class MaxHeap<E extends Comparable<E>> {
         data = new Array<>();
     }
 
+    public MaxHeap(E[] arr) {
+        data = new Array<>(arr);
+        for (int i = parent(arr.length - 1); i >= 0; i--)
+            siftDown(i);
+    }
+
     // 返回堆中元素的个数
     public int size() {
         return data.getSize();
@@ -86,4 +92,14 @@ public class MaxHeap<E extends Comparable<E>> {
             k = j;
         }
     }
+
+    // 取出堆中的最大元素，并且替换成元素e
+    public E replace(E e) {
+        E ret = findMax();
+        data.set(0, e);
+        siftDown(0);
+
+        return ret;
+    }
+
 }
